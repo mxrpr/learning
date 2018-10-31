0082d8bc92a587138c929f4b5b0d43cfcd2a1e6b
Mixer <ferenc.tollas@gmail.com>
2018-10-31 09:09:32 +0100

    leveinshtein.kt: Remove recursive solution, extend CharSequence with leveinshtein method

--- src/main/kotlin/leveinshtein.kt
+++ src/main/kotlin/leveinshtein.kt
@@ -2,26 +2,15 @@ package com.mxr.example
 
 import kotlin.system.measureNanoTime
 
+/**
+ * For learning purposes
+ *
+ * Two solutions:
+ * 1. create Leveinshtein class
+ * 2. extend CharSequence functionality
+ */
 class Leveinshtein {
 
-    // inefficient
-    fun leveinshteinDistanceRecursive(lhs: String, rhs: String): Int {
-
-        return this.calculate(lhs, rhs)
-    }
-
-    private fun calculate (lhs: String, rhs: String) : Int {
-        if (lhs.isEmpty()) return rhs.length
-        if (rhs.isEmpty()) return lhs.length
-
-        val substitute = calculate(lhs.substring(1), rhs.substring(1)) + cost(lhs[0], rhs[0])
-        val insert = calculate(lhs, rhs.substring(1)) + 1
-        val delete = calculate(lhs.substring(1), rhs) + 1
-
-        return Math.min((Math.min(substitute, insert)), delete)
-    }
-
-    // efficient
     fun leveinshteinDistance(lhs: String, rhs: String): Int {
         if (lhs === rhs) return 0
         if (lhs.isEmpty()) return rhs.length
@@ -53,20 +42,49 @@ class Leveinshtein {
     private fun cost(a: Char, b: Char ): Int = if (a == b) 0 else 1
 }
 
+/**
+ * Extend the CharSequence
+ */
+
+fun CharSequence.leveinshtein(lhs: String): Int{
+    if (this === lhs) return 0
+    if (lhs.isEmpty()) return this.length
+    if (this.isEmpty()) return lhs.length
+
+    val res = Array(lhs.length){ IntArray(this.length)}
+
+    for (i in 0 until lhs.length)
+        res[i][0] = i
+
+    for (j in 0 until this.length)
+        res[0][j] = j
+
+    var subst = 0
+    for (j in 1 until this.length) {
+        for (i in 1 until lhs.length) {
+            subst = if (lhs[i] == this[j]) 0 else 1
+
+            val deletion = res[i-1][j] + 1
+            val insertion = res[i][j-1] +1
+            val substitution = res[i-1][j-1] + subst
+            res[i][j] = Math.min(Math.min(deletion, insertion), substitution)
+        }
+    }
+
+    return res[lhs.length-1][this.length-1]
+}
+
 fun main(args: Array<String>) {
     val lev = Leveinshtein()
+
     var time = measureNanoTime {
-        println(lev.leveinshteinDistanceRecursive("rosettacode", "raisethysword"))
-    }
-    var res = time
-    println("time: $res")
-
-
-    time = measureNanoTime {
         println(lev.leveinshteinDistance("rosettacode", "raisethysword"))
     }
 
-    res = time
-    println("time: $res")
+    println("time: $time")
 
+    time = measureNanoTime {
+       println("rosettacode".leveinshtein("raisethysword"))
+    }
+    println("time: $time")
 }
